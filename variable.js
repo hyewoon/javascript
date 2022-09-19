915282a44847e43e4f62c553d81c12bf3f81071d
hyewoon <likethebegin@naver.com>
2022-09-19 12:19:29 +0900

    update file
    updat files

---
+++ variable.js
@@ -0,0 +1,18 @@
+
+//변수 선언 키워드 let vs let[변수], const[상수]
+// 1. short, int 
+
+let a = 5;//number, number() : casting
+console.log("a의 자료형: " +typeof a)
+
+let b = 5.0; //number
+console.log("b의 자료형: " +typeof b)
+
+let c ="test"; 
+
+let j =function( ) { //function
+    console.log('hello javascript world');
+}
+
+
+//일반적인 자바스크립트 타입 ==> sybol
