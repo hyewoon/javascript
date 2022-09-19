2c6d0e9cbacfd3366fe299789d0bc78414f272ee
hyewoon <likethebegin@naver.com>
2022-09-19 12:59:42 +0900

    javascript class-part2
    자바스크립트 리터럴 생성자 규칙에 대해 알라봅니다

---
+++ literal.js
@@ -0,0 +1,19 @@
+let myName = "홍길동"; //리터널
+let yourName = new String("김길동"); //생성자
+console.log(myName, yourName);
+
+let mySocore= 100;
+let yourScore = new Number(70); //생성자
+
+//객체
+let myObj = {
+    name: "선영훈",
+    gender : "남",
+    liveIn : "광산구"
+}
+
+console.log(myObj.name, myObje['gender']);
+//배열
+
+
+//문자열 
--- variable.js
+++ variable.js
@@ -1,5 +1,6 @@
 
 //변수 선언 키워드 let vs let[변수], const[상수]
+//use strict 엄격모드, js 파일 최상단에 위치 자바와 비슷하게 엄격하게 쓰려면 var대신 let쓸 수 있다 
 // 1. short, int 
 
 let a = 5;//number, number() : casting
@@ -13,6 +14,9 @@ let c ="test";
 let j =function( ) { //function
     console.log('hello javascript world');
 }
+
+
+let k = 100;
 
 
 //일반적인 자바스크립트 타입 ==> sybol
